db22460cf24df82cf98965b23f1545455494856c
suraj phuyal <surajphuyalsss3@gmail.com>
2016-06-24 11:53:10 -0700

    third commit-for program

---
+++ Cylinder Program/src/cylinder/program/CylinderProgram.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cylinder.program;
+import java.util.Scanner;
+/**
+ *
+ * @author user
+ */
+public class CylinderProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+     double radious, height;
+     System.out.println("Enter the value of radious of cylinder");
+     radious= input.nextDouble();
+      
+      System.out.println("Enter the height of cylinder");
+      height= input.nextDouble();
+       
+        System.out.print("totoal surface area of cylinder =");
+        System.out.println(2*3.14*radious*(radious+height));
+    }
+    
+}
